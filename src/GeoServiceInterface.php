9bcfe735edc8f1f4c3c194c910240bffe793b04e
Vladymyr <vovangud@gmail.com>
2022-11-02 15:38:43 +0200

    Creating interface

---
+++ src/GeoServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Hillel\GeoInterface;
+
+interface GeoServiceInterface
+{
+    public function continentCode();
+    public function countryCode();
+    public function parse($ip);
+}
